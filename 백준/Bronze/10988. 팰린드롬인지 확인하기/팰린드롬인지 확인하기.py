88fad18a139690437369a9fe3f5c1309c14ff510
swlee <swo98@naver.com>
2024-06-17 16:24:04 +0900

    Update 팰린드롬인지 확인하기.py

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,7 +1,27 @@
+# ver1
 word = list(input())
 reversed_word = list(reversed(word))
 
 if(word == reversed_word):
+    print(1)
+else:
+    print(0)
+
+# ver2
+word = input()
+
+for i in range(len(word)):
+    if(word[i] == word[-i-1]):
+        continue
+    else:
+        print(0)
+        exit(0)
+print(1)
+
+# ver3
+word = list(input())
+
+if(word == word[::-1]):
     print(1)
 else:
     print(0)
