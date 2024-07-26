afac2d147b3ca89317a9899ee25b4a07b54c57ee
swlee <swo98@naver.com>
2024-07-26 15:42:43 +0900

    Update 수들의 합 2.py

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.py
@@ -1,6 +1,10 @@
 n, target = map(int,input().split())
 nums = list(map(int, input().split()))
 
+# 누적합 구할거임
+# 정확히 몇 개를 더할지 모르기 때문에 right을 하나씩 늘려가면서 누적합 할거임
+# 그러다가 right이 n보다 커지면 끝에 도달하고 다음꺼 조회니까 그 때는 탈출
+
 left, right = 0, 1
 cnt = 0
 
