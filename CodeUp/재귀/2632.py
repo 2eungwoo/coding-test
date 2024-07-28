14d0fa1ccea2bdffaeabef7716386042e4a96677
swlee <swo98@naver.com>
2024-07-28 15:47:29 +0900

    Update 2632.py

--- CodeUp/재귀/2632.py
+++ CodeUp/재귀/2632.py
@@ -2,10 +2,15 @@ n = int(input())
 memo = [0] * (n+1)
 
 def climb(n, memo):
+  if(n == 0):
+    memo[0] = 0
+    return memo[0]
   if(n == 1):
     memo[1] = 1
+    return memo[1]
   elif(n == 2):
     memo[2] = 2
+    return memo[2]
 
   if(memo[n]):
     return memo[n]
