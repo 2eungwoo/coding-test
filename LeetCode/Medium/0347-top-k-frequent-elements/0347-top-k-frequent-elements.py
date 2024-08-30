97add476038e7fb96f88e82a80395f1924e1329a
swlee <swo98@naver.com>
2024-08-30 17:41:47 +0900

    Time: 87 ms (67.03%), Space: 21.2 MB (67.99%) - LeetHub

--- LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -4,7 +4,7 @@ class Solution:
     
         # 빈도수 많은 순서대로 정렬
         sorted_items = sorted(dic.items(), key=lambda x: x[1], reverse=True)
-        
+        #print(sorted_items)
         # 상위 k개만 뽑기
         res = []
         for i in range(k):
