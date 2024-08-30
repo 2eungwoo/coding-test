b5dce025646f1bbc5c8f4c77b1d6f1904c95d18b
swlee <swo98@naver.com>
2024-08-30 17:33:30 +0900

    Time: 75 ms (98.28%), Space: 21.3 MB (34.93%) - LeetHub

---
+++ LeetCode/Medium/0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,14 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        dic = collections.Counter(nums)
+    
+        # 빈도수 많은 순서대로 정렬
+        sorted_items = sorted(dic.items(), key=lambda x: x[1], reverse=True)
+        
+        # 상위 k개만 뽑기
+        res = []
+        for i in range(k):
+            res.append(sorted_items[i][0])
+        
+        return res
+
