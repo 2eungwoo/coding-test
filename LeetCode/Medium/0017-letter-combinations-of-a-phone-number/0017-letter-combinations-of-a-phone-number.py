b414ae6ab99a0515c13e89df85bbeb5ff56d2616
swlee <swo98@naver.com>
2024-08-31 18:22:29 +0900

    Update 0017-letter-combinations-of-a-phone-number.py

--- LeetCode/Medium/0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ LeetCode/Medium/0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -14,18 +14,19 @@ class Solution:
             "9":"wxyz"
         }
 
-        res = []
+        # digits : ex) 23 -> abc, def
+        # digits[i] -> 2, 3
+        # numpad[digits[i]] -> abc, def ... letters
 
-        def sol(index, cur_comb):
-            if len(cur_comb) == len(digits):
+        def sol(idx, cur_comb):
+            if(len(cur_comb) == len(digits)):
                 res.append(cur_comb)
                 return
-        
-            letters = numpad[digits[index]]
+
+            letters = numpad[digits[idx]]
             for letter in letters:
-                sol(index + 1, cur_comb + letter)
-    
-    
-        sol(0, "")
+                sol(idx + 1, cur_comb + letter)
+
+        res = []
+        sol(0,"") # idx, cur_combinations
         return res
-        
