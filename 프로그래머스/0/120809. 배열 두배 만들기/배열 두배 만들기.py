eba3da3bbf0b1b6857af4a9e51744553b4d4340a
swlee <swo98@naver.com>
2025-03-01 18:09:17 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.21 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -1,7 +1,8 @@
 def solution(numbers):
+    # answer = num*2 for num in numbers
     answer = []
     
     for i in range(len(numbers)):
         answer.append(numbers[i]*2)
-        
+    
     return answer
