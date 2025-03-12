80e9ea9f409bdc40ea3711bbf505fb2028f05733
swlee <swo98@naver.com>
2025-03-12 23:16:16 +0900

    [level 0] Title: A 강조하기, Time: 0.01 ms, Memory: 9.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -0,0 +1,10 @@
+def solution(myString):
+    answer = ''
+    
+    for c in myString:
+        if(c == 'a' or c == 'A'):
+            answer += c.upper()
+        else:
+            answer += c.lower()
+    
+    return answer
