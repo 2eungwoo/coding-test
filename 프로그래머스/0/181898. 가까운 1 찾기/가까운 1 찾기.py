2e8d5d320541b9367c87c78b2f42240e8a0c354e
swlee <swo98@naver.com>
2025-03-14 21:23:55 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.01 ms, Memory: 9.15 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -1,9 +1,21 @@
+# def solution(arr, idx):
+#     answer = []
+    
+#     for i,n in enumerate(arr):
+#         if(i >= idx and n == 1):
+#             answer.append(i)
+        
+#     return -1 if not answer else min(answer)
+# => O(N) + O(N)
+
 def solution(arr, idx):
-    answer = []
+    answer = -1
     
     for i,n in enumerate(arr):
         if(i >= idx and n == 1):
-            answer.append(i)
+            answer = i
+            break
         
-    
-    return -1 if not answer else min(answer)
+    return answer
+# => O(N)
+
