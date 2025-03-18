63686596e758d6d77414df979e171edd5fe1a1ca
swlee <swo98@naver.com>
2025-03-19 01:27:07 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.00 ms, Memory: 9.23 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.py
@@ -0,0 +1,13 @@
+def solution(str_list):
+    answer = []
+    
+    for i,c in enumerate(str_list):
+        if(c == 'l'):
+            answer = str_list[:i]
+            break
+        elif(c == 'r'):
+            answer = str_list[i+1:]
+            break
+            
+    return [] if not answer else answer
+            
