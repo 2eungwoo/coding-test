c7e63e46906c8cbae5b3057a30b7f33f9575ce17
swlee <swo98@naver.com>
2025-03-21 17:17:08 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: ., Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    answer = []
+    new_string = my_string.split(' ')
+    for c in new_string:
+        if(c == ''):
+            continue
+        answer.append(c)
+    return answer
