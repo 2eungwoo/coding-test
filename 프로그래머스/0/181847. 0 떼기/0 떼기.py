3bd650d9ea5f4c0e88074f0322197de2c63f8fbd
swlee <swo98@naver.com>
2025-03-21 17:22:54 +0900

    [level 0] Title: 0 떼기, Time: 0.00 ms, Memory: 9.04 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.py
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.py
@@ -1,9 +1,10 @@
 def solution(n_str):
-    answer = ''
-    idx = 0
-    for i,c in enumerate(n_str):
-        if(c != '0'):
-            idx = i
-            break
+#     answer = ''
+#     idx = 0
+#     for i,c in enumerate(n_str):
+#         if(c != '0'):
+#             idx = i
+#             break
     
-    return n_str[idx:]
+#     return n_str[idx:]
+    return n_str.lstrip('0')
