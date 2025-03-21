37d7d22ae088d30037ceee825c76d5cdcaaef4c0
swlee <swo98@naver.com>
2025-03-21 17:17:53 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.01 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.py
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.py
@@ -1,8 +1,9 @@
 def solution(my_string):
-    answer = []
-    new_string = my_string.split(' ')
-    for c in new_string:
-        if(c == ''):
-            continue
-        answer.append(c)
-    return answer
+    # answer = []
+    # new_string = my_string.split(' ')
+    # for c in new_string:
+    #     if(c == ''):
+    #         continue
+    #     answer.append(c)
+    # return answer
+    return my_string.split()
