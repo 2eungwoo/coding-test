12be3a2c60f8cb75fae7b9979773999454ac252a
swlee <swo98@naver.com>
2025-04-04 19:15:03 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.53 ms, Memory: 9.39 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -1,25 +1,52 @@
+# def solution(n):
+#     arr = [[0] * n for _ in range(n)]
+#     num = 1
+#     x, y = 0, 0
+#     dx = [0, 1, 0, -1]
+#     dy = [1, 0, -1, 0]
+#     dir = 0
+    
+#     stack = [(x, y, num, dir)]
+    
+#     while stack:
+#         x, y, num, dir = stack.pop()
+#         arr[x][y] = num # 방문
+#         num += 1
+        
+#         nx, ny = x + dx[dir], y + dy[dir]
+        
+#         if(nx < 0 or nx >= n or ny < 0 or ny >= n or arr[nx][ny] != 0):
+#             dir = (dir + 1) % 4 # 우->하->좌->상
+#             nx, ny = x + dx[dir], y + dy[dir]
+        
+#         if(num <= n*n):
+#             stack.append((nx, ny, num, dir))
+    
+#     return arr
+
 def solution(n):
     arr = [[0] * n for _ in range(n)]
-    num = 1
-    x, y = 0, 0
     dx = [0, 1, 0, -1]
     dy = [1, 0, -1, 0]
-    dir = 0
     
-    stack = [(x, y, num, dir)]
-    
-    while stack:
-        x, y, num, dir = stack.pop()
-        arr[x][y] = num # 방문
-        num += 1
-        
-        nx, ny = x + dx[dir], y + dy[dir]
-        
-        if(nx < 0 or nx >= n or ny < 0 or ny >= n or arr[nx][ny] != 0):
-            dir = (dir + 1) % 4 # 우->하->좌->상
-            nx, ny = x + dx[dir], y + dy[dir]
-        
-        if(num <= n*n):
-            stack.append((nx, ny, num, dir))
-    
+
+    def dfs():
+        stack = [(0, 0, 1, 0)]  # x,y,num,dir
+
+        while(stack):
+            x, y, num, dir = stack.pop()
+            arr[x][y] = num
+
+            if(num >= n*n):
+                continue
+
+            for i in range(4):
+                ndir = (dir + i) % 4
+                nx, ny = x + dx[ndir], y + dy[ndir]
+
+                if(0 <= nx < n and 0 <= ny < n and arr[nx][ny] == 0):
+                    stack.append((nx, ny, num + 1, ndir)) 
+                    break # 모든 방향 다 가는게 아니고 한 방향만 가는거니까 멈춰줘야됨
+
+    dfs()
     return arr
