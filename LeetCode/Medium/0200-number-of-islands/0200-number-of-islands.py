0744145f51a1f479fee361f30233053e0fceff97
swlee <swo98@naver.com>
2025-04-04 17:42:00 +0900

    Time: 249 ms (57.63%), Space: 20 MB (95.56%) - LeetHub

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.py
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.py
@@ -1,32 +1,33 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
+        rows, cols = len(grid), len(grid[0])      
+        dx = [1,0,-1,0]
+        dy = [0,-1,0,1]
+        cnt = 0
+
         if not grid:
             return 0
 
-        height = len(grid)
-        width = len(grid[0])
-        # directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        dx = [-1,0,1,0]
-        dy = [0,1,0,-1]
-        result = 0
-
-        def dfs(y, x):
-            # Mark the current land as visited
-            grid[y][x] = '0'
+        def dfs(x, y):
             
-            for dir in range(4):
-                nx = x + dx[dir]
-                ny = y + dy[dir]
-            #for dy, dx in directions:
-             #   ny, nx = y + dy, x + dx
-                if 0 <= ny < height and 0 <= nx < width and grid[ny][nx] == '1':
-                    dfs(ny, nx)
+            stack = [(x, y)]
+            
+            while(stack):
+                cur_x, cur_y = stack.pop()
+        
+                grid[cur_x][cur_y] = "V"
 
-        for i in range(height):
-            for j in range(width):
-                if grid[i][j] == '1':
-                    dfs(i, j)
-                    result += 1
+                for i in range(4):
+                    nx, ny = cur_x + dx[i], cur_y + dy[i]
+                    if(0 <= nx < rows and 0 <= ny < cols and grid[nx][ny] == "1"):
+                        stack.append((nx,ny))
 
-        return result
+    
+        for i in range(rows):
+            for j in range(cols):
+                if(grid[i][j] == "1"):
+                    dfs(i,j)
+                    cnt += 1
+
+        return cnt
