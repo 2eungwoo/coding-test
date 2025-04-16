49b7ac13f8b1aa77bd823b51e6b84cf915f0b861
swlee <swo98@naver.com>
2025-04-16 23:09:39 +0900

    [level 2] Title: 주식가격, Time: 25.58 ms, Memory: 19 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,14 +1,30 @@
 def solution(prices):
-    answer = []
+#     answer = []
+#     n = len(prices)
+    
+#     for i in range(n):
+#         diff = 0
+#         for j in range(i+1,n):
+#             diff += 1
+#             if(prices[i] > prices[j]):
+#                 break
+#         answer.append(diff)
+        
+#     return answer
     n = len(prices)
+    answer = [0] * n
+    stack = []
     
     for i in range(n):
-        diff = 0
-        for j in range(i+1,n):
-            diff += 1
-            if(prices[i] > prices[j]):
-                break
-        answer.append(diff)
+        while(stack and prices[i] < prices[stack[-1]]):
+            j = stack.pop()
+            answer[j] = i - j
+            
+        stack.append(i)
+
+    while(stack):
+        j = stack.pop()
+        answer[j] = n-j-1
         
     return answer
             
