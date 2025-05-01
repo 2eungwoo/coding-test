990a49fa9deee24377d65cae28f92aea8dad0a3f
swlee <swo98@naver.com>
2025-05-01 18:18:58 +0900

    Time: 1 ms (38.48%), Space: 17.7 MB (77.79%) - LeetHub

---
+++ LeetCode/Easy/3174-clear-digits/3174-clear-digits.py
@@ -0,0 +1,14 @@
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        stack = []
+        nums = ["1","2","3","4","5","6","7","8","9","0"]
+        
+        for ch in s:
+            stack.append(ch)
+            if(ch in nums):
+                stack.pop()
+                stack.pop()
+
+        return ''.join(stack)
+    
+
