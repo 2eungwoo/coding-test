e3c00d62da09103fc07eec839dee30eb57c4216d
Seungwoo Lee <localhost9843@gmail.com>
2025-09-08 17:35:21 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 176 ms, Memory: 15888 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int q = Integer.parseInt(br.readLine());
+
+        int n = s.length();
+        int[][] prefix = new int[n+1][26];
+
+        for(int i=1; i<=n; i++) {
+            int ch = s.charAt(i - 1) - 'a';
+            for(int j=0; j<26; j++) {
+                prefix[i][j] = prefix[i-1][j];
+            }
+            prefix[i][ch]++;
+        }
+        
+        for(int i=0; i<q; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            char c = st.nextToken().charAt(0);
+            int l = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
+
+            int idx = c - 'a';
+            int answer = prefix[r+1][idx] - prefix[l][idx];
+            System.out.println(answer);
+        }
+    }
+}
+
