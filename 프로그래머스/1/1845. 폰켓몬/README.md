d322ac8679d4d7c7461f580773c1b7228c28038c
Seungwoo Lee <localhost9843@gmail.com>
2025-09-12 17:13:02 +0900

    [level 1] Title: 폰켓몬, Time: 3.11 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/README.md
+++ 프로그래머스/1/1845. 폰켓몬/README.md
@@ -1,10 +1,10 @@
 # [level 1] 폰켓몬 - 1845 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1845) 
 
 ### 성능 요약
 
-메모리: 83 MB, 시간: 6.06 ms
+메모리: 73.3 MB, 시간: 3.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 29일 19:52:28
+2025년 09월 12일 17:12:59
 
 ### 문제 설명
 
--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,18 +1,24 @@
-import java.util.*;
+import java.util.HashSet;
 
 class Solution {
     public int solution(int[] nums) {
-    
-        Set<Integer> set = new HashSet<>();
-        for(int n : nums) {
+
+        // 중복해서 가져가지 않는다.
+        HashSet<Integer> set = new HashSet<>();
+        for (int n : nums) {
             set.add(n);
         }
-        
+
+        // 최대 nums.length/2 마리 가져갈 수 있다.
+        int half = nums.length / 2;
+
         int max = set.size();
-        
-        if(max >= (nums.length)/2) {
-            return nums.length/2;
-        }else{
+
+        // 중복 제외하고 가져갈 수 있는 최대 개수는 set.size()
+        // 단, 여기서 가져갈 수 있는 수는 half를 넘을 수 없음
+        if (max >= half) {
+            return half;
+        } else {
             return max;
         }
     }
