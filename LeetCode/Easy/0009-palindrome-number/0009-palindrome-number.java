df71f844ae370561b30b07ccbae80185f76d5f17
Seungwoo Lee <localhost9843@gmail.com>
2025-09-30 18:50:36 +0900

    Time: 5 ms (64.76%), Space: 44 MB (77.11%) - LeetHub

--- LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.java
+++ LeetCode/Easy/0009-palindrome-number/0009-palindrome-number.java
@@ -1,28 +1,35 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0) return false;
-        if(x == 0) return true;
-        
-        int len = (int)Math.log10(x) + 1;
+
+        if(x < 0) {
+            return false;
+        }
+
+        if(x == 0) {
+            return true;
+        }
+
+        int len = (int) Math.log10(x) + 1;
         int[] arr = new int[len];
 
-        for(int i=0; i<len; i++){
+        for(int i=0; i<len; i++) {
             arr[i] = x % 10;
             x /= 10;
         }
 
         int left = 0;
-        int right = arr.length - 1;
+        int right = len - 1;
 
         while(left < right) {
             if(arr[left] == arr[right]) {
-                left += 1;
-                right -= 1;
+                left++;
+                right--;
             }
             else {
                 return false;
             }
         }
+
         return true;
     }
 }
