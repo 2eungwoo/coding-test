f9547662bc2f8b4f44447d5d40273b889b6edf70
Seungwoo Lee <localhost9843@gmail.com>
2025-10-06 15:18:37 +0900

    Time: 5 ms (77.37%), Space: 57.1 MB (91.4%) - LeetHub

--- LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.java
+++ LeetCode/Medium/0011-container-with-most-water/0011-container-with-most-water.java
@@ -2,29 +2,33 @@ class Solution {
     public int maxArea(int[] height) {
         int left = 0;
         int right = height.length - 1;
-        int max = 0;
+        int water = 0;
+        int max = Integer.MIN_VALUE;
 
         while(left < right) {
-            int row = right - left;
-            int col = Math.min(height[left], height[right]);
-            int water = row * col;
-            max = Math.max(max, water);
+            int w = right - left;
+            int h = Math.min(height[left], height[right]);
+            water = w * h;
+            max = Math.max(water, max);
 
             if(height[left] < height[right]) {
                 left++;
-            }else{
+            } else {
                 right--;
             }
         }
 
         return max;
     }
+   
 }
 
-// 물 넓이 = h * w 
-// w == right - left
 
-// h == 작은 막대 기준으로 물이 참
-// -> left vs right 중 작은 곳 min(left,right)
-// 작은쪽 기준으로 물 높이를 계산하니까
-// 투포인터로 height[i] 이동하면서 작은쪽의 변경이 필요함
+// 물 높이를 구한다. 가로 x 세로
+// 가로는 right - left
+// 세로는 height[i]
+
+// height[i]가 관건인데, 이게 두개가 있어야되고 (height[left], height[right]) 중에서 길이가 작은 쪽
+// 기준으로 물이 찬다.
+// 그래서 작은쪽에 변화를 주면서(two-pointer) water volume을 게속 갱신/비교 한다음
+// 비교 다 마치면 그떄의 max변수에 있는 값을 리턴
