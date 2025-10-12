90e92d5d22481b6f62ddc91f52bf0f20389cb0ef
Seungwoo Lee <localhost9843@gmail.com>
2025-10-12 19:27:15 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 176 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        if(n == 1) {
+            System.out.println(1);
+            return;
+        }
+        
+        if(n == 2) {
+            System.out.println(1);
+            return;
+        }
+        
+        long[] dp = new long[n+1];
+        
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        
+        for(int i=3; i<=n; i++) {
+          dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        System.out.println(dp[n]);
+    }
+}
