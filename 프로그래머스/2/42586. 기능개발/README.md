5f085f7e8a6c4e4641a2c3f5042942170dcc617f
Seungwoo Lee <localhost9843@gmail.com>
2025-11-07 15:55:16 +0900

    [level 2] Title: 기능개발, Time: 2.28 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 75.4 MB, 시간: 3.24 ms
+메모리: 71.9 MB, 시간: 2.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 09월 26일 21:03:21
+2025년 11월 07일 15:55:13
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -4,21 +4,20 @@ import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int len = progresses.length;
-
-        // 1. 배포까지 걸리는 시간을 각각 구한다.
-        int[] requireDate = new int[len];
-        for (int i = 0; i < len; i++) {
-            requireDate[i] = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
+        // 일단 각 기능이 배포되는데까지 걸리는 시간을 구함
+        // 100을 넘어야되니까 (100 - p[i]) / s[i] 에다가 반올림해야됨
+        int[] require = new int[progresses.length];
+        for (int i = 0; i < require.length; i++) {
+            require[i] = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
         }
 
-        // 2. 비교한다.
+        // 소요 시간을 밀어넣으면서 비교함
         int cnt = 1;
         Stack<Integer> stack = new Stack<>();
         List<Integer> deploy = new ArrayList<>();
-        for (int i = 0; i < requireDate.length; i++) {
+        for (int i = 0; i < progresses.length; i++) {
             if (!stack.isEmpty()) {
-                if (stack.peek() < requireDate[i]) {
+                if (stack.peek() < require[i]) {
                     deploy.add(cnt);
                     cnt = 1;
                 } else {
@@ -26,31 +25,38 @@ class Solution {
                     continue;
                 }
             }
-            stack.push(requireDate[i]);
-
+            stack.push(require[i]);
         }
         deploy.add(cnt);
 
         return deploy.stream().mapToInt(i -> i).toArray();
-        
     }
 }
 
-// 각 진도와 속도에 맞춰서 배포가 가능한 날짜를 계산함
-// requireDate = ((100 - prog[i]) / speed[i]).ceil()
+// 일단 각 기능이 배포되는데까지 걸리는 시간을 구함
+// 작업이 배포까지 걸리는 시간을 require[] 라고 하겠음
+// [95,90,99,99,80,99] 에서 [1,1,1,1,1] 일 때
+// requred = [5,10,1,1,20,1] 임
+// 앞에꺼가 배포되어야 뒤에꺼가 배포됨
+// 워터폴임
+// 몇 개의 기능이 배포되는지 담는걸 deploy[] 라고 하겠음
+// 배포될 때 누적해서 cnt에 담아서 deploy 넣겠음(각 i에는 한방에 배포된 개수를 담음)
 
-// requiredDate[i] 랑 requiredDate[i+n] 이랑 비교한다?
-// r[i] < r[i+n] 이면 i~n-1개 배포 이걸 progresses.length까지?
-// r[i]돌다가.. r[i] < r[n]인게 나오면 r[i~n-1]을 push? O(n^2)
+// stack을 사용해서 프로세스를 넣고
+// 다음 프로세스랑 비교를 해서 만약 다음거가 더 크면 배포 바로 가능하니 cnt++, 스택에서 작업을 뺌
+// 만약 다음거가 더 작으면 기다려야하므로 cnt는 냅두고 push도 안함
 
-// requireDate = {5,10,1,1,20,1}
-// idx1: cnt:1 => 5 vs 10 -> 5 < 10 이므로 cnt를 push, cnt=1
-// idx2: cnt:1 => 10 vs 1 -> 10 > 1 이므로 skip, cnt++
-// idx3: cnt:2 => 10 vs 1 -> 10 > 1 이므로 skip, cnt++
-// idx4: cnt:3 => 10 vs 20 -> 10 < 20 이므로 cnt를 push, cnt=1
-// idx5: cnt:1 => 20 vs 1 -> 20 > 1 이므로 skip, cnt=1
+// 배포를 할 수 있으면 누적카운트를 deploy에 넣고, 다시 누적카운트를 1로 초기화
 
-// 마지막 cnt는 push가 안됐으니 추가로 마지막 cnt를 push
+/*
+    idx=0, 첫작업은 배포가능, 뒤에꺼랑 비교를 아직 안했으므로 작업시간만 stack에 밀어넣음 idx=1부터 비교 시작
+    idx=1, cnt=1, peek:5 < r[i]:10 이므로 배포 가능함, 배포했으니 누적 초기화, stack은 새로 기준 갱신, deploy:[1]
+    idx=2, cnt=1, peek:10 > r[i]:1 이므로 기다려야됨, 누적++만 하고 skip
+    idx=3, cnt=2, peek:10 > r[i]:1 이므로 기다려야됨, 누적++만 하고 skip
+    idx=4, cnt=3, peek:10 < r[i]:20 이므로 배포가 됨, 누적초기화, stack기준갱신, deploy[1,3]
+    idx=5, cnt=1, peek:20 > r[i]:1 이므로 기다려야됨, 누적++만 하고 skip
 
-// 대소 비교 과정에서 조건에 따라 계속 들고다니는 숫자를 컨트롤해야하므로 이거를 스택을써서
-// skip, push를 사용
+    cnt=2
+    마지막인 idx=5는 인덱스 끝나서 비교가안됨 따라서 무조건 배포 가능 deploy[1,3,2]
+*/
+
