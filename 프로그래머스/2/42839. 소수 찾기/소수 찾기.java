948458e2183501dd937fe700fd5bd3dfcde77215
Seungwoo Lee <localhost9843@gmail.com>
2025-11-08 17:35:48 +0900

    [level 2] Title: 소수 찾기, Time: 10.83 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,68 +1,77 @@
 import java.util.Set;
 import java.util.HashSet;
-import java.util.Iterator;
 
 class Solution {
     public int solution(String numbers) {
-        // 1. 숫자 조합 + set에 저장
+
         Set<Integer> numberSet = new HashSet<>();
-        
         String init = "";
         boolean[] used = new boolean[numbers.length()];
-        generateCombination(init, numbers.toCharArray(), used, numberSet);
-        
-        // 2. 소수 판별 + 맞으면 cnt++
+        generate(init, numbers, used, numberSet);
+
         int cnt = 0;
-        Iterator<Integer> it = numberSet.iterator();
-        
-        while(it.hasNext()) {
-            int num = it.next();
-            if(isPrime(num)) {
+        for (int num : numberSet) {
+            if (isPrime(num)) {
                 cnt++;
             }
         }
-        
+
         return cnt;
+
     }
     
-    private void generateCombination(
-        String cur, 
-        char[] others, 
-        boolean[] used,
-        Set<Integer> numberSet){
-        
-        // 현재 값 처리 -> 조합된 cur를 set에 넣음
-        if(!cur.equals("")) {
-              numberSet.add(Integer.valueOf(cur));
+    private void generate(String curComb, String candidates, boolean[] used, Set<Integer> numberSet) {
+
+        // 현재 값
+        if (!curComb.equals("")) {
+            numberSet.add(Integer.valueOf(curComb));    
         }
         
-        // 다음 값 처리 -> 현재에다가 아직 안쓴거를 조합함
-        // 아직 안쓴거는 others의 index와 used의 index로 조절
-        for(int i=0; i<others.length; i++) {
-            if(!used[i]) {
+        // 상태 전이
+        for (int i = 0; i < candidates.length(); i++) {
+            if (!used[i]) {
                 used[i] = true;
-                generateCombination(cur + others[i], others, used, numberSet);
+                generate(curComb + candidates.charAt(i), candidates, used, numberSet);
                 used[i] = false;
             }
+            
         }
-        
     }
-    
+
     private boolean isPrime(int num) {
-        if(num == 0 || num == 1) {
+        if (num == 0 || num == 1) {
             return false;
         }
-        
-        int sqrtNum = (int) Math.sqrt(num);
-        for(int i=2; i<=sqrtNum; i++) {
-            if(num % i == 0) {
+
+        int middle = (int) Math.sqrt(num);
+        for (int i = 2; i <= middle; i++) {
+            if (num % i == 0) {
                 return false;
             }
         }
         return true;
     }
-}
 
-// 주어진 문자열로 조합 가능한 숫자를 다 만들어본 다음 소수인지 하나하나 판별해야한다.
-// 011의 경우, 1,1이 다른 인덱스에 위치하지만 같은 숫자값이기 때문에 중복 판별의 여지가 있으므로 set에 담아서 중복된 값은 배제하도록 한다.
-// 1.숫자 다 조합하기 -> 2. 소수판별하기 -> 3. 맞으면 cnt++하기
+}
+    
+
+
+     
+
+
+/*
+    주어진 입력으로 숫자를 모두 조합해본 다음에
+    소수인지 판별해서 count해야함
+
+    근데 011 입력의 경우 인덱스가 달라도 값이 같음
+    -> 중복 판단으로 count에 오답 영향을 끼칠 수 있으므로 고려해야함
+
+    주어진 입력으로 만들 수 있는 숫자를 각각 담은 다음 
+    반복 돌면서 이게 isPrime()? 
+    근데 이 저장을 set으로 함
+*/
+
+/*
+    근데 011 같은 경우 또 11이랑 같은 경우로 본다고 했음
+    조합을 담는 set에 애초에 숫자 타입으로 넣으면 됨 -> Set<Integer> set
+*/
