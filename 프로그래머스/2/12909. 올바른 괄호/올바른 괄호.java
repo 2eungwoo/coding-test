869813016dc4c92113aad54d876b975a3cc5b082
Seungwoo Lee <localhost9843@gmail.com>
2025-11-08 17:25:09 +0900

    [level 2] Title: 올바른 괄호, Time: 21.45 ms, Memory: 54.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,27 +2,38 @@ import java.util.Stack;
 
 class Solution {
     boolean solution(String s) {
+
         Stack<Character> stack = new Stack<>();
         
-        for(char c : s.toCharArray()) {
-            if(c == '(') {
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
                 stack.push(c);
-            } else {
-                if(!stack.isEmpty()) {
-                    if(stack.peek() == '(') {
+            } else { // c == ')'
+                if (!stack.isEmpty()) {
+                    if (stack.peek() == '(') {
                         stack.pop();
-                    }
-                } else {
+                    } 
+                } else { 
                     return false;
                 }
+                
             }
         }
-        
-        if(stack.isEmpty()) {
+
+        if (stack.isEmpty()) {
             return true;
-        } else{
+        } else {
             return false;
         }
-
     }
 }
+
+/*
+    ( 가 들어오면 stack.push
+    ) 가 들어오면 무시
+        ㄴ> )가 들어왔는데 (가 있을 경우 ()가 되므로 stack.pop
+        
+    
+    만약 최초 입력이 ) 부터 시작하면 단순히 무시만하면
+    )() 이 경우에 대해서 정상괄호라고 판단하게 되므로 고려해야함
+*/
