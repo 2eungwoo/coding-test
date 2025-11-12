d48861942d3109e17fff2405b88a0ff1302e2b8b
Seungwoo Lee <localhost9843@gmail.com>
2025-11-12 19:43:34 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.78 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,53 +1,57 @@
-import java.util.*;
+import java.util.Queue;
+import java.util.LinkedList;
 
 class Solution {
-    private final static int[] dx = {0,-1,0,1};
-    private final static int[] dy = {1,0,-1,0};
+    private static int[] dx = {0,1,0,-1};
+    private static int[] dy = {1,0,-1,0};
 
     public int solution(int[][] maps) {
-        Queue<State> q = new LinkedList<>();
-        
+        Queue<State> queue = new LinkedList<>();
+        int height = maps.length; 
         int width = maps[0].length;
-        int height = maps.length;
-
         boolean[][] visited = new boolean[height][width];
         
         visited[0][0] = true;
-        q.add(new State(0,0,1));
+        queue.offer(new State(0, 0, 1));
 
-        while (!q.isEmpty()) {
-            State cur = q.poll();
+        while (!queue.isEmpty()) {
+            State cur = queue.poll();
 
             if (cur.y == height - 1 && cur.x == width - 1) {
-                return cur.dist;
+                return cur.distance;
             }
 
-            for (int dir = 0; dir < 4; dir++) {
-                int nx = cur.x + dx[dir];
-                int ny = cur.y + dy[dir];
+            for (int i = 0; i < 4; i++) {
+                int nx = cur.x + dx[i];
+                int ny = cur.y + dy[i];
 
-                if (ny < 0 || nx < 0 || ny >= height || nx >= width)
+                if (ny < 0 || ny >= height || nx < 0 || nx >= width) {
                     continue;
-                if (visited[ny][nx] || maps[ny][nx] == 0)
-                    continue;
-
-                visited[ny][nx] = true;
-                q.add(new State(ny, nx, cur.dist + 1));
+                }
+            
+                if (!visited[ny][nx] && maps[ny][nx] == 1) {
+                    queue.offer(new State(ny, nx, cur.distance + 1));
+                    visited[ny][nx] = true;
+                }
             }
         }
+        
         return -1;
-
     }
 
-    private static class State {
+    public class State {
         private final int y;
         private final int x;
-        private final int dist;
-
-        State(int y, int x, int dist) {
+        private final int distance;
+        
+        public State(final int y, final int x, final int distance) {
             this.y = y;
             this.x = x;
-            this.dist = dist;
+            this.distance = distance;
         }
     }
 }
+
+/*
+    0:벽, 1:길
+*/
