321824df93ecc5816771915fe6ff827f943139f1
Seungwoo Lee <localhost9843@gmail.com>
2025-11-13 19:06:01 +0900

    Time: 23 ms (10.79%), Space: 76.6 MB (16.42%) - LeetHub

--- LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
+++ LeetCode/Easy/0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,18 +1,14 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        
-        // 시간 O(nlogn), 공간 O(1)
-        // Arrays.sort(nums);
-        // for(int i=1; i<nums.length; i++){
-        //     if(nums[i] == nums[i-1]) return true;
-        // }
-        // return false;
+        Arrays.sort(nums);
 
-        // 시간 O(n), 공간 O(n)
-        Set<Integer> seen = new HashSet<>();
-        for(int n : nums) {
-            if(!seen.add(n)) return true;
+        int len = nums.length;
+        for(int i=1; i<len; i++) {
+            if(nums[i-1] == nums[i]) {
+                return true;
+            } 
         }
+
         return false;
     }
 }
