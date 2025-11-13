58062e978d3232d2f9336dc739f3eae71e81a6e9
Seungwoo Lee <localhost9843@gmail.com>
2025-11-13 19:50:27 +0900

    Time: 1 ms (57.99%), Space: 47.5 MB (12.13%) - LeetHub

--- LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.java
+++ LeetCode/Hard/0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -2,34 +2,46 @@ class Solution {
     public int trap(int[] height) {
         int left = 0;
         int right = height.length - 1;
-        int lmax = height[left];
+        int lmax = height[0];
         int rmax = height[right];
 
-        int waterSum = 0;
+        int water = 0;
         while(left < right) {
             lmax = Math.max(height[left], lmax);
             rmax = Math.max(height[right], rmax);
 
-            if(lmax <= rmax) { // lmax == rmax 상황에서도 그냥 물 높이만 구하면 되므로 그냥 범위에 포함시킴
-                // waterSum += min(lmax, rmax) - height[i] 로 해도 되는데
-                // lmax < rmax 면 min(lmax, rmax)는 lmax이므로 lmax - height[i]랑 똑같음
-                waterSum += lmax - height[left];
+            if(lmax < rmax) {
+                water += (lmax - height[left]);
                 left++;
             } else {
-                waterSum += rmax - height[right];
+                water += (rmax - height[right]);
                 right--;
             }
         }
-        return waterSum;
+
+        return water;
     }
 }
 
-// 양쪽에 무조건 벽이 있어야 가운데 물이 찬다.
-// 순차적으로 스캔한다고할때 현재 내가 보고있는 위치의 높이랑
-// 양 옆 벽 중에서 작은쪽을 기준으로 차이만큼 물이 찬다
-// -> water[i] = min(leftMax, rightMax) - height[i]
-// waterSum += min(lmax, rmax) - height[i]
+/*
+    좌/우 기둥 높이에서 짧은 쪽 기준으로 물이 참
+    짧은 쪽 기준으로 차는데 현재에 벽이 있을 수 있으므로 현재 높이를 또 뺴주면 됨
+    
+    기둥 중에서 젤 높은애가 있으면 걔를 기준으로 계속 비교가 될거임 
+    lmax, rmax 계속 비교 하는데
+    같으면 어느쪽을 땡겨도 상관없음
 
-// 지금 내 위치 기준에서 lmax, rmax를 각각 찾아주려면
-// 물 높이 한번 계산할때마다 n회 스캔 해야하므로 O(n^2)
-// -> 이걸 투포인터로 lmax, rmax를 갱신하면서 물 높이를 누적해준다
+    minHeight = min(lmax, rmax)
+    water[i] = minHeight(lmax or rmax) - curHeight[i]
+    
+    만약 lmax < rmax 면 minHeight는 lmax이므로 결국
+    if(lmax < rmax) {
+        water[i] = lmax - height[i]
+        left++;
+    } else {
+        water[i] = rmax - height[i]
+        right--;
+    }
+
+    
+*/
