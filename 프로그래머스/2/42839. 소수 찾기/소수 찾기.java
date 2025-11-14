4d0c0a7c47655f963c820e91c5992d64f2f69b77
Seungwoo Lee <localhost9843@gmail.com>
2025-11-14 19:52:06 +0900

    [level 2] Title: 소수 찾기, Time: 12.69 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,13 +2,17 @@ import java.util.Set;
 import java.util.HashSet;
 
 class Solution {
+    private static Set<Integer> numberSet = new HashSet<>();
+
     public int solution(String numbers) {
 
-        Set<Integer> numberSet = new HashSet<>();
         String init = "";
         boolean[] used = new boolean[numbers.length()];
-        generate(init, numbers, used, numberSet);
 
+        // 가능한 모든 숫자 조합
+        combinate(init, numbers, used);
+
+        // 조합된 리스트를 돌면서 isPrime() --> cnt++;
         int cnt = 0;
         for (int num : numberSet) {
             if (isPrime(num)) {
@@ -17,27 +21,8 @@ class Solution {
         }
 
         return cnt;
-
     }
     
-    private void generate(String curComb, String candidates, boolean[] used, Set<Integer> numberSet) {
-
-        // 현재 값
-        if (!curComb.equals("")) {
-            numberSet.add(Integer.valueOf(curComb));    
-        }
-        
-        // 상태 전이
-        for (int i = 0; i < candidates.length(); i++) {
-            if (!used[i]) {
-                used[i] = true;
-                generate(curComb + candidates.charAt(i), candidates, used, numberSet);
-                used[i] = false;
-            }
-            
-        }
-    }
-
     private boolean isPrime(int num) {
         if (num == 0 || num == 1) {
             return false;
@@ -51,27 +36,47 @@ class Solution {
         }
         return true;
     }
-
-}
     
+    private void combinate(String curComb, String candidates, boolean[] used) {
+        // 현재 값
+        // 셋에 넣음
+        if(!curComb.equals("")) {
+            numberSet.add(Integer.valueOf(curComb));
+        }
 
+        // 다음 값
+        // 조합함
+        for (int i = 0; i < candidates.length(); i++) {
+            if (!used[i]) {
+                // 다음 상태로 넘길 때 후보를 줄여줘야함
+                // 현재 조합에서 쓴 애 말고 나머지 애를 후보로 넣어야함
+                // 현재 쓴거는 체크해놓고, 다음에 또 쓸 수 있게 해야됨
+                used[i] = true;
+                combinate(curComb + candidates.charAt(i), candidates, used);
+                used[i] = false;
+            }
+        }
+    }
 
-     
-
+    
+}
 
 /*
-    주어진 입력으로 숫자를 모두 조합해본 다음에
-    소수인지 판별해서 count해야함
+    주어진 숫자로 모든 조합을 만들어봄
+    그러면서 소수가 있으면 cnt++
+    17 -> 1, 7, 17, 71
+    경우의 수 : n! 
 
-    근데 011 입력의 경우 인덱스가 달라도 값이 같음
-    -> 중복 판단으로 count에 오답 영향을 끼칠 수 있으므로 고려해야함
+    반복마다 조합한 숫자를 list에 담은 다음
+    list를 돌면서 isPrime()? --> cnt++
 
-    주어진 입력으로 만들 수 있는 숫자를 각각 담은 다음 
-    반복 돌면서 이게 isPrime()? 
-    근데 이 저장을 set으로 함
-*/
+    근데 011의 경우 011이랑 11이랑 같이 해야되니까
+    list에 넣을 때 Integer.valueOf()로 넣으면 걍 0맨앞 해결
+    11의 경우 1,1이 각각 다른 idx라서 중복되므로
+    list에 넣을 때 중복 고려 -> set으로
 
-/*
-    근데 011 같은 경우 또 11이랑 같은 경우로 본다고 했음
-    조합을 담는 set에 애초에 숫자 타입으로 넣으면 됨 -> Set<Integer> set
+
+    추적해야되는 값 :
+        numbers조합을 할거니까 어디에 해당되는 요소를 뽑을건지에 대한 idx
+        현재까지의 조합된 숫자
 */
