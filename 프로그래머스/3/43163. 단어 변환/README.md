c89932863433c77f85c42d223689340d0c5189b5
Seungwoo Lee <localhost9843@gmail.com>
2025-11-21 17:01:31 +0900

    [level 3] Title: 단어 변환, Time: 0.48 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163?gad_source=1&gad_campaignid=22199869887) 
 
 ### 성능 요약
 
-메모리: 80.7 MB, 시간: 0.39 ms
+메모리: 81.9 MB, 시간: 0.48 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 29일 17:17:42
+2025년 11월 21일 17:01:28
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,50 +1,68 @@
-import java.util.*;
+import java.util.Queue;
+import java.util.LinkedList;
 
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        Queue<Word> q = new LinkedList<>();
-        q.add(new Word(begin, 0));
+        int len = words.length;
 
-        boolean[] visited = new boolean[words.length];
+        Queue<State> queue = new LinkedList<>();
+        queue.offer(new State(begin, 0));
 
-        while (!q.isEmpty()) {
-            Word cur = q.poll();
+        boolean[] visited = new boolean[len];
+        // visited[0] = true; 
+        // begin은 words에 없는데 방문처리하니까 첫번째워드가 무조건 방문된상태임.. 
 
-            if (cur.word.equals(target)) {
-                return cur.stepCount;
+        while(!queue.isEmpty()) {
+            State cur = queue.poll();
+
+            if(cur.word.equals(target)) {
+                return cur.depth;
             }
 
-            for (int i = 0; i < words.length; i++) {
-                if (visited[i] || !isConvertable(words[i], cur.word))
-                    continue;
-
-                visited[i] = true;
-                q.add(new Word(words[i], cur.stepCount + 1));
+            for(int i=0; i<len; i++) {
+                if(!visited[i] && convertable(cur.word, words[i])) {
+                    queue.offer(new State(words[i], cur.depth + 1));
+                    System.out.println("changed word:"+words[i]);
+                    visited[i] = true;
+                } 
             }
         }
 
         return 0;
 
     }
-    
-    private static boolean isConvertable(String w1, String w2) {
-        int count = 0;
-        for (int i = 0; i < w1.length(); i++) {
-            if (w1.charAt(i) != w2.charAt(i)) {
-                count++;
+
+    public boolean convertable(String curWord, String word) {
+        int cnt = 0;
+        for(int i=0; i<curWord.length(); i++) {
+            if(curWord.charAt(i) != word.charAt(i)) { 
+                cnt++;
             }
         }
 
-        return count == 1;
-    } 
-    
-    private static class Word {
-        private final String word;
-        private final int stepCount;
+        return cnt == 1;
+    }
 
-        private Word(String word, int stepCount) {
+    public class State {
+        private final String word;
+        private final int depth;
+
+        public State(final String word, final int depth) {
             this.word = word;
-            this.stepCount = stepCount;
+            this.depth = depth;
         }
     }
 }
+
+/*
+    단어가 딱 한 글자 다른거를 찾아서 그걸로 바꾸는 식으로 반복해야함
+    hit은 words[] 중에서 hot이랑 한 글자가 다르므로 hit->hot
+    hot은 words[] 중에서 got이랑 한 글자가 다르므로 hot->dot
+    
+    words[] 글자의 첫 번쨰 요소부터 다른지 비교해서 다른게 하나면 그게 convertable임
+
+    
+    바꿀 단어를 찾는 과정을 bfs로 완전탐색한다.
+    추적해야하는 상태는 cur word와 depth
+
+*/
