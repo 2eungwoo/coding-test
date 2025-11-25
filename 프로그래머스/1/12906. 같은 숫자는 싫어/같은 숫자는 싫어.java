bfcebfc5da34c0323f15342e927405567a454f44
Seungwoo Lee <localhost9843@gmail.com>
2025-11-25 16:57:00 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 55.01 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -5,21 +5,28 @@ public class Solution {
         Stack<Integer> stack = new Stack<>();
         int len = arr.length;
 
-        for (int i = 0; i < len; i++) {
-            if (!stack.isEmpty()) {
-                if (stack.peek() == arr[i]) {
-                    continue;
-                }
+        for(int i=0; i<len; i++) {
+            if(!stack.isEmpty() && stack.peek() == arr[i]) {
+                continue;
             }
             stack.push(arr[i]);
         }
-        
+
         return stack.stream().mapToInt(i -> i).toArray();
-    }
+    }   
 }
 
-// 중복 삭제하되 순서를 유지하면서 남겨야됨 -> set은 못씀
-// 순서를 유지하기위해 arr 차례대로 순회하면서 중복 나오면 skip하는 방식으로
-// arr을 stack에 그대로 쌓으면서 중복 나오면 skip
+/*
+    중복을 제거하되, 순서를 유지해야한다.
+    단순 set에 넣으면 안된다.
+    <val,idx> 로 놓고 정렬을한다? 
+    nlogn이면 1000000000000 이라서 안됨
 
-// [4,4,4,3,3]
+    연속적으로 나타나는 숫자만 제거하면 된다.
+    따라서 첫 1이랑 끝1 이 있을 때 제거할 필요가 없고
+    첫1이랑 바로 다음1이 나온 경우에만 그 1을 스킵하면 됨
+    
+    스택을 써서 요소를 돌면서 
+    현재 탑에 있으면 스킵
+    혀재 탑에 없으면 푸쉬
+*/
