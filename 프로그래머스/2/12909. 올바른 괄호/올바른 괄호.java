4d1c23c0c9ce382084888c0b67133dd245b34f9d
Seungwoo Lee <localhost9843@gmail.com>
2025-11-25 17:28:32 +0900

    [level 2] Title: 올바른 괄호, Time: 12.63 ms, Memory: 54.5 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,39 +1,34 @@
-import java.util.Stack;
-
+// non-stack 버전
 class Solution {
     boolean solution(String s) {
 
-        Stack<Character> stack = new Stack<>();
-        
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                stack.push(c);
-            } else { // c == ')'
-                if (!stack.isEmpty()) {
-                    if (stack.peek() == '(') {
-                        stack.pop();
-                    } 
-                } else { 
-                    return false;
-                }
-                
+        int cnt = 0;
+        for(Character c : s.toCharArray()) {
+            if(c == '(') {
+                cnt++;
+            } else {
+                cnt--;
+            }
+            if(cnt < 0) {
+                return false;
             }
         }
 
-        if (stack.isEmpty()) {
-            return true;
-        } else {
-            return false;
-        }
+        return cnt == 0;
     }
 }
 
 /*
-    ( 가 들어오면 stack.push
-    ) 가 들어오면 무시
-        ㄴ> )가 들어왔는데 (가 있을 경우 ()가 되므로 stack.pop
-        
+    가장 최신의 상태만 추적하면 되므로
+    스택 없이 상수값으로도 확인할 수 있다.
+
+    만약 open이 들어오면 cnt + 1
+    close가 들어오면 cnt - 1
     
-    만약 최초 입력이 ) 부터 시작하면 단순히 무시만하면
-    )() 이 경우에 대해서 정상괄호라고 판단하게 되므로 고려해야함
+    이렇게 해서 최종 순간에 cnt가 0이 되면 올바른 쌍만 들어왔다는 뜻
+
+    근데 이 경우
+    )( 같은 케이스는 cnt가 0이되는데
+    )가 먼저 들어오는 순간에는 cnt<0 이 될 것이므로
+    그냥 바로 false쳐버림
 */
