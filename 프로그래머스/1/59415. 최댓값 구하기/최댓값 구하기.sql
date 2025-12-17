6f4835972801ad4c098339e76d154a0af1622c1c
Seungwoo Lee <localhost9843@gmail.com>
2025-12-17 19:49:41 +0900

    [level 1] Title: 최댓값 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59415. 최댓값 구하기/최댓값 구하기.sql
+++ 프로그래머스/1/59415. 최댓값 구하기/최댓값 구하기.sql
@@ -1,5 +1,8 @@
 -- 코드를 입력하세요
-select DATETIME as 시간
+# select DATETIME as 시간
+# from ANIMAL_INS
+# order by DATETIME desc
+# limit 1;
+
+select max(DATETIME) as 시간
 from ANIMAL_INS
-order by DATETIME desc
-limit 1;
