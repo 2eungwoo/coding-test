f7b4873017815a41bcb52513c0ae5708be59433d
Seungwoo Lee <localhost9843@gmail.com>
2025-12-17 20:34:15 +0900

    [level 1] Title: 한 해에 잡은 물고기 수 구하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/1/298516. 한 해에 잡은 물고기 수 구하기/한 해에 잡은 물고기 수 구하기.sql
+++ 프로그래머스/1/298516. 한 해에 잡은 물고기 수 구하기/한 해에 잡은 물고기 수 구하기.sql
@@ -1,4 +1,18 @@
 -- 코드를 작성해주세요
-SELECT COUNT(*) AS FISH_COUNT
-FROM FISH_INFO
-WHERE YEAR(TIME) = 2021
+# SELECT COUNT(*) AS FISH_COUNT
+# FROM FISH_INFO
+# WHERE YEAR(TIME) = 2021
+
+
+
+
+
+
+
+
+
+
+
+select count(ID) as FISH_COUNT
+from FISH_INFO
+where year(TIME) = 2021;
