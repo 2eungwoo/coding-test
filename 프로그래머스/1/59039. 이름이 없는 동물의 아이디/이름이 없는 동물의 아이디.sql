ff385ce82a80218ff5344513d9da82df53f8fdb1
Seungwoo Lee <localhost9843@gmail.com>
2025-12-17 18:54:58 +0900

    [level 1] Title: 이름이 없는 동물의 아이디, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/59039. 이름이 없는 동물의 아이디/이름이 없는 동물의 아이디.sql
+++ 프로그래머스/1/59039. 이름이 없는 동물의 아이디/이름이 없는 동물의 아이디.sql
@@ -1,5 +1,22 @@
--- 코드를 입력하세요
-SELECT ANIMAL_ID
-FROM ANIMAL_INS 
-WHERE NAME IS NULL
-ORDER BY ANIMAL_ID ASC;
+# -- 코드를 입력하세요
+# SELECT ANIMAL_ID
+# FROM ANIMAL_INS 
+# WHERE NAME IS NULL
+# ORDER BY ANIMAL_ID ASC;
+
+
+
+
+
+select ANIMAL_ID
+from ANIMAL_INS
+where NAME is NULL
+order by ANIMAL_ID asc
+
+
+
+
+
+
+
+
