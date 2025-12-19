05b16eb70c75b040416f26f8ca4663dc5284c690
Seungwoo Lee <localhost9843@gmail.com>
2025-12-19 20:56:40 +0900

    [level 2] Title: 모음 사전, Time: 7.61 ms, Memory: 80.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -2,33 +2,58 @@ import java.util.List;
 import java.util.ArrayList;
 
 class Solution {
-    private static List<String> dictionary = new ArrayList<>();
-    private static char[] ALPHABET = {
-        'A','E','I','O','U'
-    };
+
+    private final static Character[] ALPHA = {'A','E','I','O','U'};    
 
     public int solution(String word) {
         
-        String init = "";
-        generate(init, ALPHABET);
+        List<String> wordList = new ArrayList<>();
 
-        return dictionary.indexOf(word) + 1;
+        generate("",wordList); // curComb, wordList
+        
+        return wordList.indexOf(word) + 1;
     }
 
-    private void generate(String cur, char[] ALPHABET) {
-        if (cur.length() == 6) {
+    private void generate(String curComb, List<String> wordList) {
+        // 현재 값 
+        if(curComb.length() == 6) {
             return;
         }
 
-        if (!cur.equals("")) {
-            dictionary.add(cur);
+        if(!curComb.equals("")) {
+            wordList.add(curComb);
         }
-        
-        for (int i = 0; i < 5; i++) {
-            generate(cur + ALPHABET[i], ALPHABET);
+
+        // 다음 값
+        for(int i=0; i<5; i++) {
+           generate(curComb + ALPHA[i], wordList);
         }
     }
+
 }
+
+
+/*
+    A 
+    AA 
+    AAA 
+    AAAA 
+    AAAAA 
+    AAAAE 
+    AAAAI 
+    ...
+    UUUUO
+    UUUUU
+
+    모든 가짓수는 5^5 => 3125 
+
+    단어 리스트를 쭉 만들어놓고 
+    그 중 word가 몇 번째에 있느냐를 리턴
+    collectioins의 indexOf
+
+*/
+
+
 /*
     단어를 쭉 만들어보고, 
     word가 그 단어 리스트에 몇 번째 있느냐 구함
