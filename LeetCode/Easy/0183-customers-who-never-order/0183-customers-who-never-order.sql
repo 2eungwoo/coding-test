07d4d2049cd7544c61990ecef0f316900abda3c8
Seungwoo Lee <localhost9843@gmail.com>
2026-05-24 19:31:13 +0900

    Time: 560 ms (69.71%), Space: 0B (100%) - LeetHub

--- LeetCode/Easy/0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ LeetCode/Easy/0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,5 +1,15 @@
 /* Write your PL/SQL query statement below */
+-- select C.name as Customers
+-- from Customers C left outer join Orders O
+--     on C.id = O.customerId
+-- where O.id is null
+
+
 select C.name as Customers
-from Customers C left outer join Orders O
-    on C.id = O.customerId
-where O.id is null
+from Customers C
+where not exists (
+    select 1
+    from Orders O
+    where O.customerId = C.id
+)
+
